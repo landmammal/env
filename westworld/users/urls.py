f31a867c5e7d7f269a3d2916693158383d42077f
landmammal <evolpirate@gmail.com>
2020-05-02 10:55:09 -0500

    adding template to our users app

--- westworld/users/urls.py
+++ westworld/users/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^home', views.home, name='home')
+    url(r'^home/$', views.home, name='home')
 ]
--- westworld/users/views.py
+++ westworld/users/views.py
@@ -4,9 +4,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    response = """
-        <h1> Good Python </h1>
-    """
-    return HttpResponse(response)
+    # response = """
+    #     <h1> Good Python </h1>
+    # """
+    # return HttpResponse(response)
+    return render(request, 'users/home.html', {})
+
     
 
